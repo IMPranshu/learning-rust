507a3eb039dba8c07461d48b4634539966c40e11
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-09-04 17:31:06 +0530

    variables in rust

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -1,8 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+fn main() { // variables
+
+    // variables in RUST is different from other programming languages in general
+    // It is staticly and strongly typed
+
+    let x =4; // implicitly assigned by compiler
+    //let y: u32 = 5; // expliccitly typed
+
+    println!("x is: {} ", x);
+
+    { // different scope
+        let x = x + 2;
+        println!("x is: {}", x);
+    }
+
+    let x = x+1;
+    println!("x is: {} ", x);
+
+
+
+
+
+
 }
 
-// The main advantage of "cargo check" command is that it checks that
-// the code can be compiled without compiling the code so that we can
-// know that at that stage the code is correct
-// It is much faster than the cmd cargo compile so run this instead for checking.
+// by default in Rust all variables defined are immutable i.e. we cannot change them
+
+
+// Name shadowing means using the same variable name but with different scope
