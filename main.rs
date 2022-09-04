16730123db9af1a5ac39851d861410c2a8d99682
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-09-04 12:43:34 +0530

    Hello World

---
+++ main.rs
@@ -0,0 +1,9 @@
+fn main() { // it is the entry point of the code
+    //println is the print statement
+    // ! tells that it is a macro and not function and it takes one argument.
+
+    println!("hello world!"); // 4 spaces for indentation.  not tabs
+
+}
+
+// to run the code we run "rustc main.rs" from the terminal
