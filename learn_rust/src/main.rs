94e3ea42cec52caf01d1f2a601bcc1644f657b93
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-09-05 20:07:20 +0530

    inputs in rust

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -1,17 +1,29 @@
+// prelude is the list of things that RUST automatically imports into every program.
+// think of this as an import statement used in C++ or similar in other languages.
+
+// Note that user input is not included in the Prelude that is something unique.
+// we have to manually import that.
+
+// A package/library i.e. collection of code is known as "Crate" in Rust lang.
+// Within in package/library there are "Module" which has a specific piece of functionality
+
+use std::io; // we are using the "std" crate/package here and bringing only the "IO" module
+
+
 fn main() {
-    // array
+    // here we are creating a mutable string. String is different in Rust than other languages.
+    let mut input = String::new(); // :: is known as the "path separator operator"
 
-    // only similar type of data can be grouped together to make an array.
-    let arr = [1,3,4]; // implicit
-    // we can't add an elemnt to the ayyar, we have make a new array.
+    // we are creating a mutable reference for the input varable for modifying the data that is
+    // stored in the input variable.
+    // if we don't use &mut the read_line function will make a copy of the
+    // input variable and run all the operations on that copy and it will not get
+    // reflected in the original input variable.
 
-    let arr2: [i32; 5]=[1,2,3,4,5]; // explicit [datatype, number of elements]
+    io::stdin().read_line(&mut input).expect("error: failed to read line."); // the .expect is the method which will throw error if any input is not given or
+    // if any unexpected error occurs during the reading input.
 
-
-    let arr3: [i32,5];// uninitalized array remains like this.
-    println!("{}", arr[4]); // we will get an error when try to access the empty array element.
-
-
+    println!("{}", input);
 
 
 
