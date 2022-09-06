7aca5eca6725c08ed11dd8fb986bb13733de807d
Pranshu Agrawal <pranshukumar1999@gmail.com>
2022-09-06 06:26:30 +0530

    arithmetic op in rust

--- learn_rust/src/main.rs
+++ learn_rust/src/main.rs
@@ -1,29 +1,17 @@
-// prelude is the list of things that RUST automatically imports into every program.
-// think of this as an import statement used in C++ or similar in other languages.
-
-// Note that user input is not included in the Prelude that is something unique.
-// we have to manually import that.
-
-// A package/library i.e. collection of code is known as "Crate" in Rust lang.
-// Within in package/library there are "Module" which has a specific piece of functionality
-
-use std::io; // we are using the "std" crate/package here and bringing only the "IO" module
-
-
 fn main() {
-    // here we are creating a mutable string. String is different in Rust than other languages.
-    let mut input = String::new(); // :: is known as the "path separator operator"
+    // arithmetic
+    // default fro int is i8 and for float is f64
 
-    // we are creating a mutable reference for the input varable for modifying the data that is
-    // stored in the input variable.
-    // if we don't use &mut the read_line function will make a copy of the
-    // input variable and run all the operations on that copy and it will not get
-    // reflected in the original input variable.
+    let x = 93_02_i32;
+    let x_max = (i32::MAX as i64) + 1;
 
-    io::stdin().read_line(&mut input).expect("error: failed to read line."); // the .expect is the method which will throw error if any input is not given or
-    // if any unexpected error occurs during the reading input.
+    let y: i32 = 10;
 
-    println!("{}", input);
+    let z = x as f64 / y as f64; // explicit type conversion/
+
+    println!("{}", z);
+
+
 
 
 
